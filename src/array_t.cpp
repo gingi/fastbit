1122e841f2a821f1824dc8647fb30680b6b0e3dd
kwu <kwu@9ec3b2dc-5744-0410-9993-b8723341f1db>
2012-03-21 04:45:59 +0000

    -- make array_t::deepCopy copy empty right-hand side (address short bitvector problem reported by Teryl Taylor)
    git-svn-id: https://codeforge.lbl.gov/anonscm/fastbit/trunk@497 9ec3b2dc-5744-0410-9993-b8723341f1db

--- src/array_t.cpp
+++ src/array_t.cpp
@@ -368,9 +368,11 @@ void ibis::array_t<T>::copy(const array_t<T>& rhs) {
 } // ibis::array_t<T>::copy
 
 /// The deep copy function.  It makes an in-memory copy of @c rhs.
+/// If @c rhs is empty or uninitialized, the current content is cleared
+/// resulting in an empty array.
 template<class T> 
 void ibis::array_t<T>::deepCopy(const array_t<T>& rhs) {
-    if (rhs.actual != 0 && rhs.m_begin != 0 && rhs.m_end != 0) {
+    if (rhs.actual != 0 && rhs.m_begin != 0 && rhs.m_end != 0) { // valid rhs
 	if (actual != 0 && actual->inUse() < 2U &&
 	    actual->end() >= rhs.size() * sizeof(T) + actual->begin()) {
 	    // already has enough memory allocated, stay with it
@@ -386,6 +388,9 @@ void ibis::array_t<T>::deepCopy(const array_t<T>& rhs) {
 		tmp[i] = rhs[i];
 	    swap(tmp);
 	}
+    }
+    else { // invalid rhs, mark existing array as empty
+	m_end = m_begin;
     }
 #if defined(DEBUG) || defined(_DEBUG)
     LOGGER(ibis::gVerbose > 10)
