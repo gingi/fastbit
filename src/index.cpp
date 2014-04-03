a9d1af46e4eb28652ebb49ec0679bffd6d3184bc
kwu <kwu@9ec3b2dc-5744-0410-9993-b8723341f1db>
2014-04-03 07:08:56 +0000

    -- reverse the double deletion problem instroduced on 3/24/2014
    git-svn-id: https://codeforge.lbl.gov/anonscm/fastbit/trunk@709 9ec3b2dc-5744-0410-9993-b8723341f1db

--- src/index.cpp
+++ src/index.cpp
@@ -1406,10 +1406,8 @@ void ibis::index::clear() {
     nrows = 0;
 
     // reassign the internal storage tracking variables to null
-    if (fname != 0) {
-        delete [] fname;
-        fname = 0;
-    }
+    delete [] fname;
+    fname = 0;
     // the pointer str can only be from a file and must be managed by the
     // fileManager and can not be deleted here
     str = 0;
